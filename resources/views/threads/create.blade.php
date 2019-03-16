da56ccb880c0e28c42a71dc376030e7cfd2e98fa
fpestunov <fpestunov@gmail.com>
2019-03-16 08:28:22 +0400

    add extracting  to view composers

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -15,7 +15,7 @@
                             <label for="title">Choose a Channel:</label>
                             <select name="channel_id" id="channel_id" class="form-control" required>
                                 <option>Choose one...</option>
-                                @foreach (App\Channel::all() as $channel)
+                                @foreach ($channels as $channel)
                                     <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
                                         {{ $channel->name }}
                                     </option>
